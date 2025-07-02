df7b31d1be4a3dd7d6691904dc7e40ddbe88635c
ChaosTh3ori3 <crinker@posteo.de>
2025-07-02 15:36:09 +0200

    feat: Add initial Angular frontend structure with configuration files and basic components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatAnchor, MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButton, MatAnchor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
